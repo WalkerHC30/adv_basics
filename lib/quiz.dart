f34a2045208337d9985c4bec2da0910787e3c6bb
David_Chen_HT <vscode1005@hotaileasing.com.tw>
2024-08-25 16:42:01 +0800

    [update] 新增總結頁面, 準備開始修飾佈局前.

--- lib/quiz.dart
+++ lib/quiz.dart
@@ -1,6 +1,8 @@
 import 'package:adv_basics/questions_screen.dart';
 import 'package:flutter/material.dart';
 import 'package:adv_basics/start_screen.dart';
+import 'package:adv_basics/data/questions.dart';
+import 'package:adv_basics/results_screen.dart';
 
 const Color color1 = Color.fromARGB(255, 3, 70, 157);
 const Color color2 = Color.fromARGB(217, 107, 187, 244);
@@ -14,7 +16,8 @@ class Quiz extends StatefulWidget {
 }
 
 class _QuizState extends State<Quiz> {
-  final List<String> selectedAnswers = [];
+  // final List<String> selectedAnswers = [];
+  List<String> selectedAnswers = []; //因應重置答案要重新宣告，故不能用final
 
   // var activeScreen = const StartScreen(); 這裡會自動把activeScreen型別變為StartScreen的
   // Widget? activeScreen; //注意 宣告要用Widget放寬條件
@@ -34,14 +37,28 @@ class _QuizState extends State<Quiz> {
 
   void choiceAnswer(String answer) {
     selectedAnswers.add(answer);
+    if (selectedAnswers.length == questions.length) {
+      setState(() {
+        activeScreen = 'results-screen';
+        // selectedAnswers = [];
+      });
+    }
   }
 
   @override
   Widget build(BuildContext context) {
     Widget screenWidge = StartScreen(switchScreen);
+
     if (activeScreen == 'question-screen') {
       screenWidge = QuestionsScreen(onSelectAnswer: choiceAnswer);
     }
+
+    if (activeScreen == 'results-screen') {
+      screenWidge = ResultsScreen(
+        chosenAnswers: selectedAnswers,
+      );
+    }
+
     return MaterialApp(
       home: Scaffold(
         body: Container(
---
+++ lib/results_screen.dart
@@ -0,0 +1,42 @@
+import 'package:flutter/material.dart';
+import 'package:adv_basics/data/questions.dart';
+import 'package:adv_basics/questions_summary.dart';
+
+class ResultsScreen extends StatelessWidget {
+  const ResultsScreen({super.key, required this.chosenAnswers});
+
+  final List<String> chosenAnswers;
+
+  List<Map<String, Object>> getSummaryData() {
+    final List<Map<String, Object>> summary = []; //製作答案總結陣列
+    for (var i = 0; i < chosenAnswers.length; i++) {
+      summary.add({
+        'question_index': i,
+        'question': questions[i].text,
+        'correct_answer': questions[i].answer[0],
+        'user_answer': chosenAnswers[i]
+      });
+    }
+    return summary;
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      width: double.infinity,
+      child: Container(
+        margin: const EdgeInsets.all(40),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            const Text('You answer X out of Y question correctly!'),
+            const SizedBox(
+              height: 30,
+            ),
+            QuestionsSummary(getSummaryData()),
+          ],
+        ),
+      ),
+    );
+  }
+}
